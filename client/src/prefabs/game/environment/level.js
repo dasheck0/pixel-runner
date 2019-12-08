7f2e46ae982aecba60577cb96439480fced24590
Stefan Neidig <s.neidig@appcom-interactive.de>
2019-12-08 17:07:30 +0100

    Randomly place blocks in level

--- client/src/prefabs/game/environment/level.js
+++ client/src/prefabs/game/environment/level.js
@@ -1,4 +1,4 @@
-import { times } from 'lodash';
+import { times, sample, flattenDeep } from 'lodash';
 
 import Base from '../../base/object'
 
@@ -9,61 +9,59 @@ export default class Level extends Base {
     const type = 1;
     const size = 16;
 
-    const width = Math.trunc(this.envs.width / size);
-    const height = Math.trunc(this.envs.height / size);
+    this.width = Math.trunc(this.envs.width / size);
+    this.height = Math.trunc(this.envs.height / size);
 
     this.map = this.scene.make.tilemap({
-      width,
-      height,
+      width: this.width,
+      height: this.height,
       tileWidth: size,
       tileHeight: size
     });
 
-    console.log("HEfkdj", width, height);
-
-    const tileset = this.map.addTilesetImage('terrain', 'terrain', size, size);
-    const data = times(height, (yIndex) => times(width, (xIndex) => {
+    this.tileset = this.map.addTilesetImage('terrain', 'terrain', size, size);
+    const data = times(this.height, (yIndex) => times(this.width, (xIndex) => {
       if (xIndex === 0 && yIndex === 0) {
-        return (tileset.columns * type * 4) + 3;
+        return (this.tileset.columns * type * 4) + 3;
       }
 
-      if (xIndex === width - 1 && yIndex === 0) {
-        return (tileset.columns * type * 4) + 4;
+      if (xIndex === this.width - 1 && yIndex === 0) {
+        return (this.tileset.columns * type * 4) + 4;
       }
 
-      if (xIndex === 0 && yIndex === height - 1) {
-        return (tileset.columns * type * 4) + tileset.columns * 1 + 3;
+      if (xIndex === 0 && yIndex === this.height - 1) {
+        return (this.tileset.columns * type * 4) + this.tileset.columns * 1 + 3;
       }
 
-      if (xIndex === width - 1 && yIndex === height - 1) {
-        return (tileset.columns * type * 4) + tileset.columns * 1 + 4;
+      if (xIndex === this.width - 1 && yIndex === this.height - 1) {
+        return (this.tileset.columns * type * 4) + this.tileset.columns * 1 + 4;
       }
 
       if (xIndex === 0) {
-        return (tileset.columns * type * 4) + tileset.columns * 1 + 2;
+        return (this.tileset.columns * type * 4) + this.tileset.columns * 1 + 2;
       }
 
-      if (xIndex === width - 1) {
-        return (tileset.columns * type * 4) + tileset.columns * 1;
+      if (xIndex === this.width - 1) {
+        return (this.tileset.columns * type * 4) + this.tileset.columns * 1;
       }
 
       if (yIndex === 0) {
-        return (tileset.columns * type * 4) + tileset.columns * 2 + 1;
+        return (this.tileset.columns * type * 4) + this.tileset.columns * 2 + 1;
       }
 
-      if (yIndex === height - 1) {
-        return (tileset.columns * type * 4) + 1;
+      if (yIndex === this.height - 1) {
+        return (this.tileset.columns * type * 4) + 1;
       }
 
       return -1;
     }));
 
-    this.backgroundLayer = this.map.createBlankDynamicLayer('background', tileset, 0, 0, width, height);
+    this.backgroundLayer = this.map.createBlankDynamicLayer('background', this.tileset, 0, 0, this.width, this.height);
     this.backgroundLayer.setDepth(1);
     this.backgroundLayer.putTilesAt(data, 0, 0);
 
-    this.backgroundLayer.getTilesWithin(0, 0, width, height).forEach((tile, index) => {
-      const blocked = index < width || index >= width * (height - 1) || (index % width === 0) || (index % width === (width - 1));
+    this.backgroundLayer.getTilesWithin(0, 0, this.width, this.height).forEach((tile, index) => {
+      const blocked = index < this.width || index >= this.width * (this.height - 1) || (index % this.width === 0) || (index % this.width === (this.width - 1));
 
       tile.properties.blocked = blocked;
       tile.properties.unblocked = !blocked;
@@ -72,5 +70,46 @@ export default class Level extends Base {
         tile.setCollision(true, true, true, true, this.backgroundLayer);
       }
     });
+
+    times(10, () => this.addRandomBlock(null, 2));
+  }
+
+  addRandomBlock(blockType, textureType) {
+    this.addBlock(Phaser.Math.Between(1, this.width - 1), Phaser.Math.Between(5, this.height - 1), blockType, textureType);
+  }
+
+  addBlock(x, y, blockType, textureType = 0) {
+    const formations = [[1, 1, 12 + this.tileset.columns * 1], [1, 3, 15], [3, 1, 12], [2, 2, 13 + this.tileset.columns * 1]];
+    const formation = formations[blockType] || sample(formations);
+
+    const positions = [{
+      x,
+      y,
+      data: formation[2] + this.tileset.columns * 4 * textureType
+    }];
+
+    for (let xi = 0; xi < formation[0]; xi += 1) {
+      for (let yi = 0; yi < formation[1]; yi += 1) {
+        positions.push({
+          x: x + xi,
+          y: y + yi,
+          data: formation[2] + xi + this.tileset.columns * yi + this.tileset.columns * 4 * textureType
+        });
+      }
+    }
+
+    this._addBlock(positions)
+  }
+
+  _addBlock(positions) {
+    positions.forEach((position) => {
+      if (position.x > 0 && position.x < this.width && position.y > 0 && position.y < this.height) {
+        let tile = this.backgroundLayer.getTileAt(position.x, position.y);
+        if (!tile || tile.unblocked) {
+          tile = this.backgroundLayer.putTileAt(position.data, position.x, position.y);
+          tile.setCollision(true, true, true, true);
+        }
+      }
+    })
   }
 }
